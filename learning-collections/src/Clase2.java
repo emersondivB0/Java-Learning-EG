b0a37a35c8df803f5de486ee4649f6aa7a5c8fcc
Emerson PC <emerson.dvlmt@gmail.com>
2023-03-16 23:06:42 -0300

    Avance de Collections (lists)

---
+++ learning-collections/src/Clase2.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+public class Clase2 {
+	
+	public static void main(String[] args) {
+		String curso1 = "Geometría";
+		String curso2 = "Física";
+		String curso3 = "Química";
+		String curso4 = "Historia";
+		
+		ArrayList<String> cursos = new ArrayList<>();
+		cursos.add(curso1);
+		cursos.add(curso2);
+		cursos.add(curso3);
+		cursos.add(curso4);
+		/*
+		System.out.println(cursos);
+		
+		Collections.sort(cursos);
+		
+		System.out.println(cursos);
+		
+		Collections.sort(cursos, Collections.reverseOrder());
+		
+		System.out.println(cursos);
+		*/
+		
+		cursos.sort(Comparator.reverseOrder());
+		
+		System.out.println(cursos);
+	}
+
+}
