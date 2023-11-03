436f0b0d8b55581e6953aaddc26b8f7d26b67f2e
Emerson Gonzalez <emerson.dvlmt@gmail.com>
2023-11-03 06:06:25 -0300

    Second Leetcode exercise

---
+++ Basics/3-Arrays/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int[] prices = {2, 4, 1};
+        Solution solution = new Solution();
+        int maxProfit = solution.maxProfit(prices);
+        System.out.println("Max Profit: " + maxProfit);
+
+    }
+}
